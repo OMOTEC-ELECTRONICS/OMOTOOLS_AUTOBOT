972c249a4cb43788555440074ab42245ffcb61c9
OMOTEC <151024042+OMOTEC-ELECTRONICS@users.noreply.github.com>
2025-01-30 14:43:30 +0530

    Add files via upload

---
+++ 5maze_Game/mazegame_autobot.ino
@@ -0,0 +1,41 @@
+#include <SoftwareSerial.h>
+SoftwareSerial Bluetooth(4,5); // RX | TX
+int button1= 6;	 //button connected at D6
+int button2= 7;	//button connected at D7
+int button3= 8;	//button connected at D8
+int button4= 9;	//button connected at D9
+
+
+
+void setup()
+{
+  Serial.begin(9600);
+  Bluetooth.begin(9600);	 //bluetooth communication
+  pinMode(6,INPUT);	// button as input
+  pinMode(7,INPUT);	// button as input
+  pinMode(8,INPUT);	// button as input
+  pinMode(9,INPUT);	// button as input
+  
+ 
+}
+
+void loop()
+{
+   int sensor_value1= digitalRead(button1);// reading the values
+   int sensor_value2= digitalRead(button2);// reading the values
+   int sensor_value3= digitalRead(button3);// reading the values
+   int sensor_value4= digitalRead(button4);// reading the values
+   delay(3000);
+   
+   Serial.print(sensor_value1);	//display on serial monitor
+   Serial.print(sensor_value2); 	//display on serial monitor
+   Serial.print(sensor_value3); 	//display on serial monitor
+   Serial.println(sensor_value4); 	//display on serial monitor
+   
+   Bluetooth.print(sensor_value1);// send the data through bluetooth
+   Bluetooth.print(sensor_value2);// send the data through bluetooth
+   Bluetooth.print(sensor_value3);// send the data through bluetooth
+   Bluetooth.println(sensor_value4);// send the data through bluetooth
+
+   
+}
