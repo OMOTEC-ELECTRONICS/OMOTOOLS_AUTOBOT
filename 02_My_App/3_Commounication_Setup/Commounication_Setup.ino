06d0ece56c2eb02debc61eb3e849187e1aac027e
OMOTEC <151024042+OMOTEC-ELECTRONICS@users.noreply.github.com>
2025-06-10 19:54:19 +0530

    Add files via upload

---
+++ 02_My_App/3_Commounication_Setup/Commounication_Setup.ino
@@ -0,0 +1,23 @@
+#include <SoftwareSerial.h>
+char BT_input;
+SoftwareSerial BlueTooth(4, 5);
+void setup() {
+  // put your setup code here, to run once:
+Serial.begin(9600);
+BlueTooth.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if (BlueTooth.available())
+  {
+   BT_input=BlueTooth.read();
+   Serial.write(BT_input);
+  }
+  if (Serial.available())
+  {
+    BT_input = Serial.read();
+   BlueTooth.write(BT_input);
+  
+}
+}
